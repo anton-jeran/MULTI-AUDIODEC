0be50e152b08c96cf74db677ef35e25c8ff494ec
Anton Jeran Ratnarajah <73730558+anton-jeran@users.noreply.github.com>
2023-08-23 17:59:01 -0700

    Add files via upload

---
+++ Single_Multi_AudioDec/download_model.sh
@@ -0,0 +1,2 @@
+gdown https://drive.google.com/uc?id=10bQADjGMwar1BbXrxysznRVg8_F9HQUh
+unzip exp.zip
---
+++ Single_Multi_AudioDec/download_test_data.sh
@@ -0,0 +1,3 @@
+gdown https://drive.google.com/uc?id=1iS1QAGsU175fs9KgXuLjjFPKKGWQvo8j
+unzip output_speech.zip
+
