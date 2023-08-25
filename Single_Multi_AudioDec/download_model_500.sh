17e32eee1e9984d0e3d9dda4a5f83f85ac4e3461
Anton Jeran Ratnarajah <73730558+anton-jeran@users.noreply.github.com>
2023-08-25 16:29:25 -0700

    Add files via upload

--- Single_Multi_AudioDec/download_model_500.sh
+++ Single_Multi_AudioDec/download_model_500.sh
@@ -1,4 +1,8 @@
 gdown https://drive.google.com/uc?id=1Ln-Q8cvi_H_X7Xp06J4-lAsf0QSSBtww
-zip -FF exp.zip -O exp_fixed.zip
-unzip exp_fixed.zip
-rm -rf exp.zip exp_fixed.zip
+7z e exp.zip 
+mv checkpoint-500000steps.pkl symAD_vctk_48000_hop300
+mv symAD_vctk_48000_hop300 autoencoder
+mv autoencoder exp
+mv *pkl AudioDec_v1_symAD_vctk_48000_hop300_clean
+mv AudioDec_v1_symAD_vctk_48000_hop300_clean vocoder
+mv vocoder exp
