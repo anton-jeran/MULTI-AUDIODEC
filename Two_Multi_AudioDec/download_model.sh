58032e9e6e2af837430bf1a9cde8975fe9e6b825
Anton Jeran Ratnarajah <73730558+anton-jeran@users.noreply.github.com>
2023-08-25 13:44:19 -0700

    Update download_model.sh

--- Two_Multi_AudioDec/download_model.sh
+++ Two_Multi_AudioDec/download_model.sh
@@ -1,2 +1,4 @@
-gdown https://drive.google.com/uc?id=1jfx5Pfr3zwrgU1gBZmeaEO7pMp0R7WPP
-unzip exp.zip
+gdown https://drive.google.com/uc?id=1qB-ZTr37r1Vf5GweAhHr_2_9sRZZRb8m
+zip -FF exp.zip -O exp_fixed.zip
+unzip exp_fixed.zip
+rm -rf exp.zip exp_fixed.zip
