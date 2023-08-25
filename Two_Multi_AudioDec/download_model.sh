1124bff7db0aa23a07ba9c985c6f264393bd26dd
Anton Jeran Ratnarajah <73730558+anton-jeran@users.noreply.github.com>
2023-08-25 16:29:10 -0700

    Add files via upload

--- Two_Multi_AudioDec/download_model.sh
+++ Two_Multi_AudioDec/download_model.sh
@@ -1,4 +1,5 @@
 gdown https://drive.google.com/uc?id=1qB-ZTr37r1Vf5GweAhHr_2_9sRZZRb8m
-zip -FF exp.zip -O exp_fixed.zip
-unzip exp_fixed.zip
-rm -rf exp.zip exp_fixed.zip
+7z e exp.zip 
+mv *pkl symAD_vctk_48000_hop300
+mv symAD_vctk_48000_hop300 autoencoder
+mv autoencoder exp
