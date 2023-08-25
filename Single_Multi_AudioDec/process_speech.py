4f40740f814edb86c86113923f45e778920dd594
Anton Jeran Ratnarajah <73730558+anton-jeran@users.noreply.github.com>
2023-08-25 11:36:40 -0700

    Add files via upload

--- Single_Multi_AudioDec/process_speech.py
+++ Single_Multi_AudioDec/process_speech.py
@@ -28,3 +28,42 @@ for files in file_list_train:
 	
 
 	sf.write(full_path,speech,fs)
+
+
+file_list_test = os.listdir(test_path)
+
+for files in file_list_test:
+	full_path = test_path + files
+
+	speech,fs = sf.read(full_path)
+
+	crop_length = 96000
+	length = speech.shape[0]
+	if(length<crop_length):
+		zeros = np.zeros(crop_length-length)
+		speech = np.concatenate([speech,zeros])
+	else:
+		speech = speech[0:96000]
+
+	
+
+	sf.write(full_path,speech,fs)
+
+file_list_valid = os.listdir(valid_path)
+
+for files in file_list_valid:
+	full_path = valid_path + files
+
+	speech,fs = sf.read(full_path)
+
+	crop_length = 96000
+	length = speech.shape[0]
+	if(length<crop_length):
+		zeros = np.zeros(crop_length-length)
+		speech = np.concatenate([speech,zeros])
+	else:
+		speech = speech[0:96000]
+
+	
+
+	sf.write(full_path,speech,fs)
