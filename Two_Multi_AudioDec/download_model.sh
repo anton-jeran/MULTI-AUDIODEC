5cbbde7ca5371e634089aeee5df66e7e7792de3e
Anton Jeran Ratnarajah <73730558+anton-jeran@users.noreply.github.com>
2023-08-25 17:05:12 -0700

    Update download_model.sh

--- Two_Multi_AudioDec/download_model.sh
+++ Two_Multi_AudioDec/download_model.sh
@@ -1,5 +1,3 @@
 gdown https://drive.google.com/uc?id=1qB-ZTr37r1Vf5GweAhHr_2_9sRZZRb8m
 7z e exp.zip 
-mv *pkl symAD_vctk_48000_hop300
-mv symAD_vctk_48000_hop300 autoencoder
-mv autoencoder exp
+mv *pkl exp/autoencoder/symAD_vctk_48000_hop300/
